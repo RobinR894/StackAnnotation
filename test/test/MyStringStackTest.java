a5e627e0c3ef89bc26615802ccf673e14439d161
RobinR894 <robin.ribette@ncs.com.sg>
2020-04-14 15:52:59 +0800

    unstable the test case

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -21,7 +21,7 @@ public class MyStringStackTest extends TestCase {
 	}
 	@Before
 	public void setUp() throws Exception {
-		s1  "Hello";
+		s1 = "Hello";
 		s2 = "there";	
 	}
 
@@ -31,13 +31,14 @@ public class MyStringStackTest extends TestCase {
 
 	@Test
 	public void testPush() {
-		String o1 = null;  
-		MyStringStack stack = new MyStringStack();
-		assertTrue (stack.isEmpty());
- 		stack.push (s1);
-        	stack.push (s2);
-        	assertFalse (stack.isEmpty());
-	}
+        String o1 = null;
+        MyStringStack stack = new MyStringStack();
+        stack.push (s1);
+        assertTrue (stack.isEmpty());
+         stack.push (s1);
+            stack.push (s2);
+            assertFalse (stack.isEmpty());
+    }
 
 	@Test
 	public void testPop() {
